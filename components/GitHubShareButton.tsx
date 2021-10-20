80cbe23449ffe36b2df1f62bb81a9cdef30e7042
NOISE <46085246+rcy1314@users.noreply.github.com>
2021-10-21 00:39:01 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const GitHubShareButton: React.FC = () => {
-
+  return (
+    <a
+      href='https://noisework.vercel.app'
+      target='_blank'
+      rel='noopener noreferrer'
+      className={styles.githubCorner}
+      aria-label='View source on GitHub'
+    >
       <svg
         width='80'
         height='80'
