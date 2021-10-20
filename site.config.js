62f8f8bd088b51f875d8cdf0944a6a58c872073d
NOISE <46085246+rcy1314@users.noreply.github.com>
2021-10-20 23:26:18 +0800

    1

---
+++ site.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '69a25fc6e04b471eac64c8757a2c2bae',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Noise&blog',
+  domain: 'www.noisezld.cn',
+  author: '我的自留地',
+
+  // open graph metadata (optional)
+  description: 'Noise&blog',
+  socialImageTitle: 'Noise&blog',
+  socialImageSubtitle: 'Hello World! 👋',
+
+  // social usernames (optional)
+  twitter: '良文豪',
+  github: 'roise1314',
+  linkedin: '',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
--- source/me/index.md
+++
@@ -1,9 +0,0 @@
----
-title: about
-date: 2020-09-14 15:30:30 
-type: "about"
-layout: "about"
----
-
-
-
